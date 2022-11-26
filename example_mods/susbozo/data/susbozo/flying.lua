674af9ae44c2387896073d2396f062a46853a410
AFatto <94957716+thefatos@users.noreply.github.com>
2022-11-26 11:36:33 +0100

    fresh 0.6.3

--- example_mods/susbozo/data/susbozo/flying.lua
+++
@@ -1,9 +0,0 @@
-function onUpdate(elapsed)
-	songPos = getSongPosition()
-	local currentBeat = (songPos/5000)*(curBpm/60)
-	if curStep >= 10 then
-	  songPos = getSongPosition()
-	  local currentBeat = (songPos/1000)*(bpm/300)
-	  doTweenY(dadTweenY, 'dad', 290-130*math.sin((currentBeat*0.25)*math.pi),0.001)
-	end
-end
